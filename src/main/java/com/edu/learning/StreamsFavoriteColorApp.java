38deddab41f00cd117d5dd86136f83a89478594f
Eduwer <ceduwer@gmail.com>
2020-08-08 17:45:24 -0500

    Procesadores para obtener color favorito

--- src/main/java/com/edu/learning/StreamsFavoriteColorApp.java
+++ src/main/java/com/edu/learning/StreamsFavoriteColorApp.java
@@ -8,16 +8,23 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Named;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 
 public class StreamsFavoriteColorApp {
 
+    private static final Set<String> ALLOWED_COLORS = new HashSet<>(Arrays.asList("BLUE", "RED", "GREEN"));
+
 
     public static void main(String[] args) {
         Properties properties = new Properties();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "favorite-colors-app");
         properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
@@ -25,10 +32,14 @@ public class StreamsFavoriteColorApp {
 
         StreamsBuilder streamsBuilder = new StreamsBuilder();
 
-        KStream<Object, Object> streamSource = streamsBuilder.stream("users-color");
-
-        streamSource.to("favorite-colors");
-
+        KStream<String, String> streamSource = streamsBuilder.stream("users-color");
+        KTable<String, Long> count = streamSource.filter((key, value) -> value != null)
+                .mapValues((readOnlyKey, value) -> value.toUpperCase())
+                .filter((key, value) -> ALLOWED_COLORS.contains(value))
+                .selectKey((key, value) -> value)
+                .groupByKey()
+                .count(Named.as("colors-count"));
+        count.toStream().to("favorite-colors");
         Topology build = streamsBuilder.build();
 
         System.out.println(build.describe());
